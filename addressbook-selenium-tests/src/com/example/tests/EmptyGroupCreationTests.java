b1f13cdc423ddf96f0dd818e64252ea66800efc7
MMarianna <m-chka@mail.ru>
2014-07-30 00:09:18 +0300

    Code structuring  HW-1
    Tests for creation of empty and not empty group are added

---
+++ addressbook-selenium-tests/src/com/example/tests/EmptyGroupCreationTests.java
@@ -0,0 +1,17 @@
+package com.example.tests;
+
+import org.testng.annotations.Test;
+
+public class EmptyGroupCreationTests extends TestBase {
+
+	@Test
+	public void testEmptyGroupCreation() throws Exception {
+		openMainPage();
+		gotoGroupsPage();
+		initGroupCreation();
+		fillGroupForm(new GroupData("", "", ""));
+		submitGroupCreation();
+		returnToGroupsPage();
+	}
+
+}
