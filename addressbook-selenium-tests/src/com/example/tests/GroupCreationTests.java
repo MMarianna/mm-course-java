ecc894669dfb09ca450fcd308be37b02e8020273
MMarianna <m-chka@mail.ru>
2014-07-30 17:26:24 +0300

    Code structuring HW-2
    Tests for creation of contact is added

--- addressbook-selenium-tests/src/com/example/tests/GroupCreationTests.java
+++ addressbook-selenium-tests/src/com/example/tests/GroupCreationTests.java
@@ -1,5 +1,6 @@
 package com.example.tests;
 
+import org.openqa.selenium.By;
 import org.testng.annotations.Test;
 
 public class GroupCreationTests extends TestBase {
@@ -14,8 +15,29 @@ public class GroupCreationTests extends TestBase {
 		group.header = "header 1";
 		group.footer = "footer 1";
 		fillGroupForm(group);
-		submitGroupCreation();
+		submitCreation();
 		returnToGroupsPage();
+	}
+
+	private void gotoGroupsPage() {
+		driver.findElement(By.linkText("groups")).click();
+	}
+
+	private void initGroupCreation() {
+		driver.findElement(By.name("new")).click();
+	}
+
+	private void fillGroupForm(GroupData group) {
+		driver.findElement(By.name("group_name")).clear();
+		driver.findElement(By.name("group_name")).sendKeys(group.name);
+		driver.findElement(By.name("group_header")).clear();
+		driver.findElement(By.name("group_header")).sendKeys(group.header);
+		driver.findElement(By.name("group_footer")).clear();
+		driver.findElement(By.name("group_footer")).sendKeys(group.footer);
+	}
+
+	private void returnToGroupsPage() {
+		driver.findElement(By.linkText("group page")).click();
 	}
 
 }
--- addressbook-selenium-tests/src/com/example/tests/TestBase.java
+++ addressbook-selenium-tests/src/com/example/tests/TestBase.java
@@ -15,7 +15,7 @@ import org.testng.annotations.BeforeTest;
 
 public class TestBase {
 
-	private static WebDriver driver;
+	protected static WebDriver driver;
 	private static String baseUrl;
 	private static boolean acceptNextAlert = true;
 	private static StringBuffer verificationErrors = new StringBuffer();
@@ -27,35 +27,6 @@ public class TestBase {
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 	}
 
-	protected void returnToGroupsPage() {
-		driver.findElement(By.linkText("group page")).click();
-	}
-
-	protected void submitGroupCreation() {
-		driver.findElement(By.name("submit")).click();
-	}
-
-	protected void fillGroupForm(GroupData group) {
-		driver.findElement(By.name("group_name")).clear();
-		driver.findElement(By.name("group_name")).sendKeys(group.name);
-		driver.findElement(By.name("group_header")).clear();
-		driver.findElement(By.name("group_header")).sendKeys(group.header);
-		driver.findElement(By.name("group_footer")).clear();
-		driver.findElement(By.name("group_footer")).sendKeys(group.footer);
-	}
-
-	protected void initGroupCreation() {
-		driver.findElement(By.name("new")).click();
-	}
-
-	protected void gotoGroupsPage() {
-		driver.findElement(By.linkText("groups")).click();
-	}
-
-	protected void openMainPage() {
-		driver.get(baseUrl + "/addressbookv4.1.4/");
-	}
-
 	@AfterTest
 	public void tearDown() throws Exception {
 		driver.quit();
@@ -63,6 +34,14 @@ public class TestBase {
 		if (!"".equals(verificationErrorString)) {
 			fail(verificationErrorString);
 		}
+	}
+
+	protected void openMainPage() {
+		driver.get(baseUrl + "/addressbookv4.1.4/");
+	}
+
+	protected void submitCreation() {
+		driver.findElement(By.name("submit")).click();
 	}
 
 	private boolean isElementPresent(By by) {
